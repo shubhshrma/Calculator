6ca65f58dc8d941ce6d52fe75780d8ed17cd769b
shubhshrma <shubham.31996@gmail.com>
2017-07-13 01:14:15 +0530

    Updated Files

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 $(document).ready(function(){
 	
 	function updateScreen(value){
-		if($("#screen").text()==='0')
-			$("#screen").html(value);
+		if($("#result").text()==='0' && !value.match(/[+\-*/%\.]/))
+			$("#result").html(value);
 		else	
-		$("#screen").append(value);
+		$("#result").append(value);
 	}
 
 
 	function update(value){
-		var text=$("#screen").text();
+		var text=$("#result").text();
+		
 		if(Number(value)>=0 && Number(value)<=9 )
 			updateScreen(value);
+		
 		else if(value.match(/[+\-*/%\.=]/))
 		{
-			
 			
 			if((value)=='.'  ){
 				if(text.search(/\./)!==-1 ){
@@ -27,25 +28,30 @@ $(document).ready(function(){
 
 			else if( text.search(/[+\-*/%=]/)!==-1 ){
 				if(value=='=')
-					$("#screen").html(eval(text));
+					$("#result").html(eval(text));
 				else if(text[text.length-1].match(/[+\-*/%]/))
 				{}
 				else
-				$("#screen").html(eval(text)+value);
+				$("#result").html(eval(text)+value);
 
 			}
+
+			else if(value=="=" && text.search(/=/)==-1)
+			{
+			}
+
 			else
 				updateScreen(value);
 
 							
 		}
 		else if(value=='ac')
-			$("#screen").html(0);
+			$("#result").html(0);
 		else if(value=='ce'){
 			if(text.length==1)
-				$("#screen").html(0);
+				$("#result").html(0);
 			else
-			$("#screen").html(text.substring(0,text.length-1));
+			$("#result").html(text.substring(0,text.length-1));
 			
 		}
 
